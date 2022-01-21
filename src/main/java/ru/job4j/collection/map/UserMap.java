b84962f2ebddb0b87ecb3ae1453dd1f7053ca26d
Arrcady555 <parfenov7233@gmail.com>
2022-01-21 23:33:14 +0300

    3. Переопределить только hashCode [#1003 #253945]

--- src/main/java/ru/job4j/collection/map/UserMap.java
+++ src/main/java/ru/job4j/collection/map/UserMap.java
@@ -1,22 +1,17 @@
 package ru.job4j.collection.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class UserMap {
 
     public static void main(String[] args) {
-        User user = new User("Arcady", 3,
-                new GregorianCalendar(1972, Calendar.NOVEMBER, 4));
-        User otherUser = new User("Arcady", 3,
-                new GregorianCalendar(1972, Calendar.NOVEMBER, 4));
-
+        User user = new User("Arcady", 3, new GregorianCalendar(1972,
+                Calendar.NOVEMBER, 4, 22, 23, 24));
+        User otherUser = new User("Arcady", 3, new GregorianCalendar(1972,
+                Calendar.NOVEMBER, 4, 22, 23, 24));
         Map<User, Object> map = new HashMap<>();
         map.put(user, new Object());
         map.put(otherUser, new Object());
-
         System.out.println(map);
     }
 }
