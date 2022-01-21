59db1cf07b4817f257ff601ddd053c81368ab377
Arrcady555 <parfenov7233@gmail.com>
2022-01-21 14:30:12 +0300

    2. Без переопределения equals и hashCode [#1005]

---
+++ src/main/java/ru/job4j/collection/map/UserMap.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserMap {
+
+    public static void main(String[] args) {
+        User user = new User("Arcady", 3
+                , new GregorianCalendar(1972, Calendar.NOVEMBER, 4));
+        User otherUser = new User("Arcady", 3
+                , new GregorianCalendar(1972, Calendar.NOVEMBER, 4));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user, new Object());
+        map.put(otherUser, new Object());
+
+        System.out.println(map);
+    }
+}
