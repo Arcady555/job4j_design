9fe5cc70c33aa574434da8d8e280d7c25d14957f
Arrcady555 <parfenov7233@gmail.com>
2022-02-22 15:05:32 +0300

    FileOutputStream. [#252488 #267862]-2

--- src/main/java/ru/job4j/io/MultiplicationTable.java
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -1,30 +1,29 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.util.Arrays;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+        int[][] data = multiple(9);
         try (FileOutputStream out = new FileOutputStream("MultiplicationTable.txt")) {
-            out.write("1 2 3 4 5 6 7 8 9".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("2 4 6 8 10 12 14 16 18").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("3 6 9 12 15 18 21 24 27").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("4 8 12 16 20 24 28 32 36").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("5 10 15 20 25 30 35 40 45").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("6 12 18 24 30 36 42 48 54").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("7 14 21 28 35 42 49 56 63").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("8 16 24 32 40 48 56 64 72").getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write(("9 18 27 36 45 54 63 72 81").getBytes());
-            out.write(System.lineSeparator().getBytes());
+            for (int[] datum : data) {
+                out.write(Arrays.toString(datum).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static int[][] multiple(int size) {
+        int[][] data = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                data[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return data;
     }
 }
