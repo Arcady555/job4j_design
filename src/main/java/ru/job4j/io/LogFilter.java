7a7b00224fe68632ced2d5810a066322477b64fe
Arrcady555 <parfenov7233@gmail.com>
2022-02-25 10:57:34 +0300

    BufferedReader. [#252489]-3

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,24 +2,30 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class LogFilter {
-    public void filter(String file) {
+
+    public List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
         final String string = "404";
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             Predicate<String> predicate = s -> {
                 String[] strings = s.split(" ");
                 return (string.equals(strings[strings.length - 2]));
             };
-            bufferedReader.lines().filter(predicate).forEach(System.out :: println);
+            list = bufferedReader.lines().filter(predicate).toList();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return list;
     }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
-        logFilter.filter("log.txt");
+        List<String> log = logFilter.filter("log.txt");
+        log.forEach(System.out :: println);
     }
 }
