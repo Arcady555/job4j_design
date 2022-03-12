df77fea4b37eea3217bf17dbf2a5e32bfc9fb136
Arrcady555 <parfenov7233@gmail.com>
2022-03-12 15:27:16 +0300

    Поиск дубликатов [#315066]-2

--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -5,19 +5,50 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
+    Set<FileProperty> set = new HashSet<>();
+    List<Path> listGen = new ArrayList<>();
+    List<Path> listRsl = new ArrayList<>();
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-        Set<FileProperty> set = new HashSet<>();
+        FileProperty fileProperty = toFileProp(file);
+        listGen.add(file);
         set.add(fileProperty);
-        for (FileProperty path : set) {
-            System.out.println(path);
+        if (!set.add(fileProperty)) {
+           addFileIfPresent(listRsl, file);
+           if (!addFileIfPresent(listRsl, file)) {
+               for (Path path : listGen) {
+                   if (path.equals(file)) {
+                       listRsl.add(path);
+                   }
+               }
+               listRsl.add(file);
+           }
         }
         return super.visitFile(file, attrs);
+    }
+
+    private FileProperty toFileProp(Path path) {
+        return new FileProperty(path.toFile().length(), path.toFile().getName());
+    }
+
+    private boolean addFileIfPresent(List<Path> list, Path path) {
+        boolean rsl = false;
+        FileProperty fp = toFileProp(path);
+        for (int i = 0; i < list.size(); i++) {
+            if (toFileProp(list.get(i)).equals(fp)) {
+                listRsl.add(i, path);
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
     }
 }
