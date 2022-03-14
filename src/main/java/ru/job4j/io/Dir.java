54330d22de6538261bdca935728c43e9110e3a2f
Arrcady555 <parfenov7233@gmail.com>
2022-03-14 22:23:40 +0300

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,17 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/home/arcady1/projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. File extension not specified. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER, FILE_EXTENSION");
+        }
+
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension not specified. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER, FILE_EXTENSION");
+        }
+        File file = new File(args[0]);
+        String string = args[1];
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
@@ -12,7 +22,9 @@ public class Dir {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + ", size: " + subfile.length());
+            if (subfile.getAbsolutePath().endsWith(string)) {
+                System.out.println(subfile);
+            }
         }
     }
 }
