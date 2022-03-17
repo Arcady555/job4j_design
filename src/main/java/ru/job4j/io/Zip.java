11e0957ff7189e60703251c4078d1dfad63c3a6e
Arrcady555 <parfenov7233@gmail.com>
2022-03-17 09:12:47 +0300

    Архивировать проект [#861 #276262]-2

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,11 +10,11 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File source : sources) {
-                zip.putNextEntry(new ZipEntry(source.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -23,54 +23,36 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void validation(String[] args, Path path) {
+        ArgsName argsName = ArgsName.of(args);
+
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Usage "
+                    + "java -jar target/zip.jar ROOT_FOLDER FILE_EXTENSION TARGET_FOLDER");
+        }
+
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", path));
+        }
+
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", path));
+        }
+
+        if (!argsName.get("e").startsWith(".")) {
+            throw new IllegalArgumentException("File-not-for-archive extension not specified");
         }
     }
 
     public static void main(String[] args) throws IOException {
+        Zip zip = new Zip();
         ArgsName argsName = ArgsName.of(args);
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. "
-                    + "File-not-for-archive extension not specified. Target folder is null."
-                    + "Usage java -jar target/zip.jar ROOT_FOLDER FILE_EXTENSION TARGET_FOLDER");
-        }
-
-        if (args.length == 1) {
-            throw new IllegalArgumentException("File-not-for-archive extension not specified."
-                    + " Target folder is null."
-                    + "Usage java -jar target/zip.jar ROOT_FOLDER FILE_EXTENSION TARGET_FOLDER");
-        }
-
-        if (args.length == 2) {
-            throw new IllegalArgumentException(" Target folder is null."
-                    + "Usage java -jar target/zip.jar ROOT_FOLDER FILE_EXTENSION TARGET_FOLDER");
-        }
-
         Path sourcesFolder = Path.of(argsName.get("d"));
-
-        if (!sourcesFolder.toFile().exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", sourcesFolder));
-        }
-        if (!sourcesFolder.toFile().isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s", sourcesFolder));
-        }
-
+        zip.validation(args, sourcesFolder);
         String extension = argsName.get("e");
         Predicate<Path> predicate = p -> !p.getFileName().endsWith(extension);
         File target = new File(argsName.get("o"));
-        List<Path> pathList = new ArrayList<>(Search.search(sourcesFolder, predicate));
-        List<File> fileList = new ArrayList<>();
-        for (Path path : pathList) {
-            fileList.add(path.toFile());
-        }
-        Zip zip = new Zip();
-        zip.packFiles(fileList, target);
+        List<Path> list = new ArrayList<>(Search.search(sourcesFolder, predicate));
+        zip.packFiles(list, target);
     }
 }
