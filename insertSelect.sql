3bc4d0c98d18633342ec5da99ef88fa3b6294625
Arcady555 <79311106+Arcady555@users.noreply.github.com>
2022-04-29 12:50:38 +0300

    Add files via upload

--- insertSelect.sql
+++ insertSelect.sql
@@ -13,11 +13,22 @@ create table lovers(
 insert into orientations(orientation) values ('getero');
 insert into orientations(orientation) values ('homo');
 insert into orientations(orientation) values ('bi');
-insert into lovers(sex, name, height, weight, orientation_id) values (true, 'Arcasha', 172, 80, 1);
-insert into lovers(sex, name, height, weight, orientation_id) values (false, 'Masha', 175, 70, 3);
-insert into lovers(sex, name, height, weight, orientation_id) values (true, 'Elton John', 160, 90, 2);
+insert into lovers(sex, name, height, weight, orientation_id)
+ values (true, 'Arcasha', 172, 80, 1);
+insert into lovers(sex, name, height, weight, orientation_id)
+ values (false, 'Masha', 175, 70, 3);
+insert into lovers(sex, name, height, weight, orientation_id)
+ values (true, 'Elton John', 160, 90, 2);
 
-select * from lovers join orientations on lovers.orientation_id = orientations.id;
-select * from orientations join lovers on lovers.orientation_id = orientations.id;
-select l.name as  Имя, l.sex as Пол, o.orientation as Ориентация from lovers as l join orientations as o on l.orientation_id = o.id;
-select o.orientation as Ориентация, l.name as Имя, l.height as Рост, l.weight as Вес from orientations as o join lovers as l on l.orientation_id = o.id;
+select * from lovers join orientations 
+on lovers.orientation_id = orientations.id;
+
+select * from orientations join lovers 
+on lovers.orientation_id = orientations.id;
+
+select l.name as  Имя, l.sex as Пол, o.orientation as Ориентация
+ from lovers as l join orientations as o on l.orientation_id = o.id;
+ 
+select o.orientation as Ориентация,
+ l.name as Имя, l.height as Рост, l.weight as Вес
+  from orientations as o join lovers as l on l.orientation_id = o.id;
