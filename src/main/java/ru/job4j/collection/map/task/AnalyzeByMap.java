5a3bb24b94eb0d756613bde9da77c169d176a7a5
Arrcady555 <parfenov7233@gmail.com>
2022-07-25 23:05:35 +0300

    Аттестация [#504878]

---
+++ src/main/java/ru/job4j/collection/map/task/AnalyzeByMap.java
@@ -0,0 +1,80 @@
+package ru.job4j.collection.map.task;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double score = 0;
+        int count = 0;
+        for (Label label : averageScoreByPupil(pupils)) {
+            score += label.score();
+            count++;
+        }
+        return score / count;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> list = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double score = 0;
+            int count = 0;
+            for (Subject subject : pupil.subjects()) {
+                score += subject.score();
+                count++;
+            }
+            list.add(new Label(pupil.name(), score / count));
+        }
+        return list;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        List<Label> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        int count = 0;
+        for (Pupil pupil : pupils) {
+            putMap(map, pupil);
+            count++;
+        }
+        for (String name : map.keySet()) {
+            list.add(new Label(name, (double) map.get(name) / count));
+        }
+        return list;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> list = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double score = 0;
+            for (Subject subject : pupil.subjects()) {
+                score += subject.score();
+            }
+            list.add(new Label(pupil.name(), score));
+        }
+        list.sort(Comparator.naturalOrder());
+        return list.get(list.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        List<Label> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (Pupil pupil : pupils) {
+            putMap(map, pupil);
+        }
+        for (String name : map.keySet()) {
+            list.add(new Label(name, (double) map.get(name)));
+        }
+        list.sort(Comparator.naturalOrder());
+        return list.get(list.size() - 1);
+    }
+
+    private static void putMap(Map<String, Integer> map, Pupil pupil) {
+        for (Subject subject : pupil.subjects()) {
+            int generalScore = 0;
+            if (map.get(subject.name()) != null) {
+                generalScore = map.get(subject.name());
+            }
+            generalScore += subject.score();
+            map.put(subject.name(), generalScore);
+        }
+    }
+}
